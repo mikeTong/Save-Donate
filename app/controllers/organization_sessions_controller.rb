35e689198ae6f216c22a38118e2df2458f7557e1
Lei Chen <lei.chen.scu@gmail.com>
2015-07-18 11:36:00 -0700

    Fix

--- app/controllers/organization_sessions_controller.rb
+++ app/controllers/organization_sessions_controller.rb
@@ -3,9 +3,9 @@ class OrganizationSessionsController <  Devise::SessionsController
     @organization = Organization.find_by_email(params[:organization][:email])
     if !@organization
       warden.custom_failure!
-      render json: {error: "The email is already taken."} 
+      render json: {error: "Invalid email or password."} 
     elsif @organization.valid_password?(params[:organization][:password])
-      render :json => {success: true, organization: {email:@organization.email, authentication_token: @organization.authentication_token}}, status: :created
+      render :json => {success: true, organization: {id: @organization.id, email:@organization.email, authentication_token: @organization.authentication_token}}, status: :created
     else
       warden.custom_failure!
       render json: {error: "Invalid email or password."}
--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,6 +1,16 @@
 class OrganizationsController < Devise::RegistrationsController
   skip_before_action :authenticate_scope!
 
+  def create 
+    @organization = Organization.new(organization_params)
+    if @organization.save
+      render :json => {success: true, message: "Account created!", organization: {id: organization.id, email: organization.email, authentication_token: organization.authentication_token}}
+    else
+      warden.custom_failure!
+      render :json => {success: false, message: "Something went wrong!"}, status: 422
+    end
+  end
+
   def destroy
     @organization = Organization.find_by_email(request.headers['X-Organization-Email'])
     if @organization && @organization.authentication_token == request.headers['X-Organization-Token']
@@ -18,5 +28,10 @@ class OrganizationsController < Devise::RegistrationsController
     else
       render json: {error: "You need to sign in or sign up before continuing."}
     end    
+  end
+
+  protected 
+
+  def organization_params
   end
 end
